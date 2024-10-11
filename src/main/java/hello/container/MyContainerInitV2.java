261723fe24c3e3c8da1e6f2bbba7f8f62b1c2754
Oh <osm723@gmail.com>
2024-10-11 15:53:17 +0900

    스프링 부트 - 핵심 원리와 활용 섹션 3. 웹 서버와 서블릿 컨테이너 정리

---
+++ src/main/java/hello/container/MyContainerInitV2.java
@@ -0,0 +1,30 @@
+package hello.container;
+
+import jakarta.servlet.ServletContainerInitializer;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.HandlesTypes;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Set;
+
+@HandlesTypes(AppInit.class)
+public class MyContainerInitV2 implements ServletContainerInitializer {
+    @Override
+    public void onStartup(Set<Class<?>> c, ServletContext ctx) throws ServletException {
+        System.out.println("MyContainerInitV1.onStartup");
+        System.out.println("c = " + c);
+        System.out.println("ctx = " + ctx);
+
+        for (Class<?> appInitClass : c) {
+            try {
+                // enw
+                AppInit appInit = (AppInit) appInitClass.getDeclaredConstructor().newInstance();
+                appInit.onStartup(ctx);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
+}
